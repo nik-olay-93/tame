9a0f7f32b789c543ca4407b2e74153a97d7043bd
Nikolay Yaroshenko <nik.olay.93@mail.ru>
2022-11-06 19:16:25 +0300

    menu button press animation

--- components/ui/AppFooter.tsx
+++ components/ui/AppFooter.tsx
@@ -3,6 +3,7 @@
 import CustomIcon from "./CustomIcon";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import styles from "./AppFooter.module.css";
 
 const routes = [
   {
@@ -31,19 +32,20 @@ export default function BottomMenu() {
   const pathname = usePathname();
 
   return (
-    <div className="flex justify-around p-1 bg-primary-light dark:bg-[#424242]">
+    <nav className="flex justify-around px-4 py-1 bg-primary-light dark:bg-[#424242]">
       {routes.map((r, i) => (
         <Link
           href={r.path}
           key={i}
-          className={`flex flex-col items-center transition-colors ease-in active:bg-[rgba(255,255,255,0.05)] rounded-full px-1 ${
+          className={`flex flex-col items-center px-1 rounded-md ${
             r.path === pathname ? "text-accent" : "text-gray-400"
-          }`}
+          } ${styles.ripple} 
+          `}
         >
           <CustomIcon icon={r.icon} fontSize="30px" />
           <span className="text-sx">{r.title}</span>
         </Link>
       ))}
-    </div>
+    </nav>
   );
 }
