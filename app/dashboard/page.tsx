9523529182c81ce7d6767422771c6c5e5d25be56
Nikolay Yaroshenko <nik.olay.93@mail.ru>
2022-11-14 00:12:10 +0300

    Dashboard data fetching

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,36 @@
-export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+import client from "lib/prismadb";
+import { getServerSession } from "lib/serverSession";
+
+async function getData() {
+  const session = await getServerSession();
+  if (!session?.body?.user?.email) {
+    return null;
+  }
+
+  const projects = await client.project.findMany({
+    where: {
+      members: {
+        some: {
+          email: session.body.user.email,
+        },
+      },
+    },
+  });
+
+  return projects;
+}
+
+export default async function DashboardPage() {
+  const data = await getData();
+
+  return (
+    <div>
+      {data?.map((project) => (
+        <div key={project.id}>
+          <h1>{project.name}</h1>
+          <p>{project.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
